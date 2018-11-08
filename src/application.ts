601a9810e17c0d8cdc61febe72a12ce577ecc8fb
Joao Polo <joaozito.polo@gmail.com>
2018-11-08 01:36:36 -0200

    maps auth lib

--- src/application.ts
+++ src/application.ts
@@ -1,20 +1,34 @@
-import {BootMixin} from '@loopback/boot';
-import {ApplicationConfig} from '@loopback/core';
-import {RepositoryMixin} from '@loopback/repository';
-import {RestApplication} from '@loopback/rest';
-import {ServiceMixin} from '@loopback/service-proxy';
-import {MySequence} from './sequence';
+import { BootMixin } from '@loopback/boot';
+import { ApplicationConfig } from '@loopback/core';
+import { RepositoryMixin } from '@loopback/repository';
+import { RestApplication } from '@loopback/rest';
+import { ServiceMixin } from '@loopback/service-proxy';
+import { MySequence } from './sequence';
+
+import {
+  AuthenticationComponent,
+  AuthenticationBindings,
+} from '@loopback/authentication';
+import { MyAuthStrategyProvider } from './providers/auth-strategy.provider';
 
 export class Lb_4AuthTestApplication extends BootMixin(
   ServiceMixin(RepositoryMixin(RestApplication)),
 ) {
   constructor(options: ApplicationConfig = {}) {
     super(options);
+    this.projectRoot = __dirname;
+
+
+    this.component(AuthenticationComponent);
+    this.bind(AuthenticationBindings.STRATEGY).toProvider(
+      MyAuthStrategyProvider,
+    );
 
     // Set up the custom sequence
     this.sequence(MySequence);
 
-    this.projectRoot = __dirname;
+
+
     // Customize @loopback/boot Booter Conventions here
     this.bootOptions = {
       controllers: {
---
+++ src/controllers/who-am-i.controller.ts
@@ -0,0 +1,19 @@
+import {inject} from '@loopback/context';
+import {
+  AuthenticationBindings,
+  UserProfile,
+  authenticate,
+} from '@loopback/authentication';
+import {get} from '@loopback/rest';
+
+export class WhoAmIController {
+  constructor(
+    @inject(AuthenticationBindings.CURRENT_USER) private user: UserProfile,
+  ) {}
+
+  @authenticate('BasicStrategy')
+  @get('/whoami')
+  whoAmI(): string {
+    return this.user.id;
+  }
+}
